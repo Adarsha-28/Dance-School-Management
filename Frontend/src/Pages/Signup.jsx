60dcc74835e0b1ec350b5707a0f2013f1d91c349
Adarsha Uthayakumar <adarsha.u2024cse@sece.ac.in>
2026-06-05 22:50:27 +0530

    Update API endpoint for registration

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -67,7 +67,7 @@ function Signup() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://groovix-78ic.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
